c78ca3f2fbe1719dc3b56333da5ba92f5ba01163
Cassemiro <admergcass@gmail.com>
2023-03-02 00:42:32 -0300

    first commit

--- ProcessSelection/src/Faturamento/Faturamento.java
+++ ProcessSelection/src/Faturamento/Faturamento.java
@@ -7,6 +7,7 @@ public class Faturamento {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		CalcFaturamento calc = new CalcFaturamento();
+		
 		double SP = 67836.43;
 		double RJ = 36678.66;
 		double MG = 29229.88;
@@ -24,7 +25,7 @@ public class Faturamento {
 		System.out.printf("MG %.2f / ", (float) calc.calc(MG));
 		System.out.printf("ES %.2f / ", (float) calc.calc(ES));
 		System.out.printf("Outros %.2f", (float) (calc.calc(Outros)));
-
+		
 		scanner.close();
 
 	}
